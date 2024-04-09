fca0936f1a1307e0e84925235bc1eae6a596c88c
kkt09072 <ksb450424@naver.com>
2024-04-09 10:41:22 +0900

    pl/sql package

--- plsql3.sql
+++ plsql3.sql
@@ -75,9 +75,21 @@ EXEC cur_super(2004);
 
 
 -- 패키지(PACKAGE) : 여러 개의 프로시저 또는 함수 등을 하나의 그룹으로 묶은 묶음.
+-- 패키지 선언부(PACKAGE DECLARATION)
+CREATE OR REPLACE PACKAGE emp_pack
+IS
+    PROCEDURE eno_out;
+    PROCEDURE ename_out;
+    PROCEDURE pno_out;
+    PROCEDURE pos_out;
+END emp_pack;    
+/
+
+-- 패키지 기능 정의부(PACKAGE DEFINE OF FUNCTION)
 CREATE OR REPLACE PACKAGE BODY emp_pack 
 IS
     CURSOR sw_cur IS SELECT * FROM emp;
+    
     PROCEDURE eno_out
     IS
     BEGIN
@@ -102,7 +114,7 @@ IS
         DBMS_OUTPUT.PUT_LINE('부서번호');
         DBMS_OUTPUT.PUT_LINE('------');
         FOR k IN sw_cur LOOP
-            DBMS_OUTPUT.PUT_LINE(K.부서번호);
+            DBMS_OUTPUT.PUT_LINE(k.pno);
         END LOOP;
     END pno_out;
     PROCEDURE pos_out
